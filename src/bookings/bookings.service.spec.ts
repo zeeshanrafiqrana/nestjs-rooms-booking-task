d99f72dd3c97d11e94b5398504be4bacbcd9b60c
zeeshanrafiqrana <zeeshanrafiqrana01@gmail.com>
2024-06-05 00:36:40 +0500

    jest basic structure and unit test cases

--- src/bookings/bookings.service.spec.ts
+++ src/bookings/bookings.service.spec.ts
@@ -1,15 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { BookingsService } from './bookings.service';
+import { Booking } from './entities/booking.entity';
+import { Room } from '../rooms/entities/room.entity';
 
 describe('BookingsService', () => {
   let service: BookingsService;
+  let bookingRepository: Repository<Booking>;
+  let roomRepository: Repository<Room>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BookingsService],
+      providers: [
+        BookingsService,
+        {
+          provide: getRepositoryToken(Booking),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Room),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<BookingsService>(BookingsService);
+    bookingRepository = module.get<Repository<Booking>>(getRepositoryToken(Booking));
+    roomRepository = module.get<Repository<Room>>(getRepositoryToken(Room));
   });
 
   it('should be defined', () => {
--- src/rooms/rooms.service.spec.ts
+++ src/rooms/rooms.service.spec.ts
@@ -1,15 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { RoomsService } from './rooms.service';
+import { Room } from './entities/room.entity';
 
 describe('RoomsService', () => {
   let service: RoomsService;
+  let repository: Repository<Room>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoomsService],
+      providers: [
+        RoomsService,
+        {
+          provide: getRepositoryToken(Room),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<RoomsService>(RoomsService);
+    repository = module.get<Repository<Room>>(getRepositoryToken(Room));
   });
 
   it('should be defined', () => {
